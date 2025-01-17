f63a45ec283c2c9553f023accac94a3fe4ec3b2a
Fifixex <122852085+Fifixex@users.noreply.github.com>
2025-01-17 03:43:17 +0000

    🔧 chore: add path alias to tsconfig and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
 	plugins: [injectHTML()],
 	root: 'src',
+	publicDir: '../public',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	build: {
+		emptyOutDir: true,
+		outDir: '../dist',
+	},
 });
